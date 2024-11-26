c95b62f0ce120211887aa20133dde1c6411ec8c9
Pablirry <pablo.lopez02@csioviedo.com>
2024-11-26 21:11:14 +0100

    .

--- JDBC_Actividad1/src/Project/Prueba.java
+++ JDBC_Actividad1/src/Project/Prueba.java
@@ -2,7 +2,6 @@ package Project;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
